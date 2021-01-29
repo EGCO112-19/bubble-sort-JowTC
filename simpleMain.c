c4231dea3ccde856553d4823f2ca1f9f002dd3f3
JowTC <tanakritchanplakorn@gmail.com>
2021-01-29 08:37:17 +0000

    change

--- simpleMain.c
+++ simpleMain.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
+
 #include <stdlib.h>
 #include "bubble.h"
 
 int main(int argc, char * argv[]){
+  printf("There are %d argument\n",argc);
     int *a, N, i;
-    printf("How many numbers:");
-    scanf("%d",&N);
-    a = (int*)malloc(N*sizeof(int));
-    for(i = 0; i<N; i++){
-        printf("Enter integer of array: ");
-        scanf("%d", a+i);
+    for(i=0;i<argc;i++){
+      printf("argv %d:%s\n",i,argv[i]);
+
     }
-    bubble(a,N);
+    N=argc-1;
+    a = (int*)malloc(N*sizeof(int));
+    for(i = 1; i<argc; i++){
+        
+        a[i-1]=atoi(argv[i]);
+    //atoiคือเปลี่ยนค่าจาก argument เป็น integer
+    }
+    //bubble(a,N);
     return 0;
 }
